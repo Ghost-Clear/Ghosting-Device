30a9b19bff9ec0d61c34d418316f1e334367188b
varunchitturi <varunchitturi@icloud.com>
2020-07-17 00:07:02 -0700

    Revert "Use phototransistor for detection instead of sound"
    This reverts commit c64a9efc2d6aea91b63011c7a68fe06475fef13e.

--- GhostingDevice_CL/src/bleuart.ino
+++ GhostingDevice_CL/src/bleuart.ino
@@ -2,17 +2,21 @@
 #include <Adafruit_LittleFS.h>
 #include <InternalFileSystem.h>
 #include <Arduino.h>
+#include "NewPing.h"
+#define TRIGGER_PIN 15
+#define ECHO_PIN 7
+#define MAX_DISTANCE 400
+NewPing sonar (TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+float distance;
 // BLE Service
 BLEDfu  bledfu;  // OTA DFU service
 BLEDis  bledis;  // device information
 BLEUart bleuart; // uart over ble
 BLEBas  blebas;  // battery
-int count = 0;
-int previous;
+
 void setup()
 {
   Serial.begin(9600);
-  pinMode(PIN_A1,INPUT);
   pinMode(PIN_A0, OUTPUT);
   digitalWrite(PIN_A0, LOW);
 #if CFG_DEBUG
@@ -32,14 +36,14 @@ void setup()
 
   Bluefruit.begin();
   Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  //Bluefruit.setName("CL");
   char name[18];
   name[0] = 'C';
   name[1] = 'L';
   for(int i = 2; i < 18;i++){
     name[i] = getMcuUniqueID()[i-2];
   }
-  Bluefruit.setName(name); 
-  //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
+  Bluefruit.setName(name); // useful testing with multiple central connections
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
 
@@ -91,19 +95,13 @@ void startAdv(void)
 
 void loop()
 {
-  int current = analogRead(PIN_A1);
-  if (count == 0){
-    previous = current;
-  }
-  if (abs(previous - current) >= 22){
-    previous = current;
-    count ++;
-  }
-  if (count % 2 == 0){
+  distance = sonar.ping_cm();
+  delay(5);
+  if (distance < 180){
     if (digitalRead(PIN_A0) == HIGH){
       bleuart.write("detected");
       digitalWrite(PIN_A0,LOW);
-      count ++;
+
     }
     
     delay(600);
